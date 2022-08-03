3b506f271de93deb7311a017dc7cd88f0a1532f1
gaomin <gaomin@bixin.cn>
2022-08-03 18:40:54 +0800

    访问者模式

---
+++ design-pattern/src/main/java/example/example/behavior/memento/InputText.java
@@ -0,0 +1,57 @@
+package example.example.behavior.memento;
+
+
+import java.util.Scanner;
+import java.util.Stack;
+
+//为了能用快照恢复 InputText 对象，我们在 InputText 类中定义了 setText() 函数，但这个函数有可能会被其他业务使用，所以，暴露不应该暴露的函数违背了封装原则；
+//快照本身是不可变的，理论上讲，不应该包含任何 set() 等修改内部状态的函数，但在上面的代码实现中，“快照“这个业务模型复用了 InputText 类的定义，而 InputText 类本身有一系列修改内部状态的函数，所以，用 InputText 类来表示快照违背了封装原则。
+public class InputText {
+    private StringBuilder text = new StringBuilder();
+
+    public String getText() {
+        return text.toString();
+    }
+
+    public void append(String input) {
+        text.append(input);
+    }
+
+    public void setText(String text) {
+        this.text.replace(0, this.text.length(), text);
+    }
+}
+
+class SnapshotHolder {
+    private Stack<InputText> snapshots = new Stack<>();
+
+    public InputText popSnapshot() {
+        return snapshots.pop();
+    }
+
+    public void pushSnapshot(InputText inputText) {
+        InputText deepClonedInputText = new InputText();
+        deepClonedInputText.setText(inputText.getText());
+        snapshots.push(deepClonedInputText);
+    }
+}
+
+class ApplicationMain {
+    public static void main(String[] args) {
+        InputText inputText = new InputText();
+        SnapshotHolder snapshotsHolder = new SnapshotHolder();
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String input = scanner.next();
+            if (input.equals(":list")) {
+                System.out.println(inputText.getText());
+            } else if (input.equals(":undo")) {
+                InputText snapshot = snapshotsHolder.popSnapshot();
+                inputText.setText(snapshot.getText());
+            } else {
+                snapshotsHolder.pushSnapshot(inputText);
+                inputText.append(input);
+            }
+        }
+    }
+}
---
+++ design-pattern/src/main/java/example/example/behavior/memento/InputText1.java
@@ -0,0 +1,69 @@
+package example.example.behavior.memento;
+
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class InputText1 {
+    private StringBuilder text = new StringBuilder();
+
+    public String getText() {
+        return text.toString();
+    }
+
+    public void append(String input) {
+        text.append(input);
+    }
+
+    public Snapshot createSnapshot() {
+        return new Snapshot(text.toString());
+    }
+
+    public void restoreSnapshot(Snapshot snapshot) {
+        this.text.replace(0, this.text.length(), snapshot.getText());
+    }
+}
+
+class Snapshot {
+    private String text;
+
+    public Snapshot(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+}
+
+class SnapshotHolder1 {
+    private Stack<Snapshot> snapshots = new Stack<>();
+
+    public Snapshot popSnapshot() {
+        return snapshots.pop();
+    }
+
+    public void pushSnapshot(Snapshot snapshot) {
+        snapshots.push(snapshot);
+    }
+}
+
+ class ApplicationMain1 {
+    public static void main(String[] args) {
+        InputText1 inputText = new InputText1();
+        SnapshotHolder1 snapshotsHolder = new SnapshotHolder1();
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String input = scanner.next();
+            if (input.equals(":list")) {
+                System.out.println(inputText.toString());
+            } else if (input.equals(":undo")) {
+                Snapshot snapshot = snapshotsHolder.popSnapshot();
+                inputText.restoreSnapshot(snapshot);
+            } else {
+                snapshotsHolder.pushSnapshot(inputText.createSnapshot());
+                inputText.append(input);
+            }
+        }
+    }
+}
